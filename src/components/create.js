1b790a0bf9b37b61398210ecc0e43ca92adf2f42
David Fitzharris <g00386045@gmit.ie>
2021-10-06 13:05:19 +0100

    Card added to create

--- src/components/create.js
+++ src/components/create.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 //Creating a Create class and marked for export
 export class Create extends React.Component {
     render() {
         return (
             <div>
-                <h1>This is the create page</h1>  
+                <h1>This is the create page</h1>
+
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>This is a card</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">Bootstrap</Card.Subtitle>
+                        <Card.Text>
+                            Below example of linking back to pages
+                        </Card.Text>
+                        <Card.Link href="/">Home</Card.Link>
+                        <Card.Link href="/read">Read</Card.Link>
+                    </Card.Body>
+                </Card>
             </div>
         );
     }
